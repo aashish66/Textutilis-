f0b24e9e417141ee0721df0dc9b55585d7df6cb0
aashish66 <85101674+aashish66@users.noreply.github.com>
2024-01-11 15:13:22 +0545

    Updates

---
+++ precache-manifest.8c553e3ecd36aa023dedb6539e659534.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d4037af0e57a0b3945ba94c2cd57bc9",
+    "url": "/Textutilis-/index.html"
+  },
+  {
+    "revision": "9e5a466db40114f36964",
+    "url": "/Textutilis-/static/css/main.6d3fb0f8.chunk.css"
+  },
+  {
+    "revision": "702d9a6e081d1421623e",
+    "url": "/Textutilis-/static/js/2.8b3be0e5.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/Textutilis-/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "9e5a466db40114f36964",
+    "url": "/Textutilis-/static/js/main.0c197187.chunk.js"
+  },
+  {
+    "revision": "528d6fb2663d4d899ce5",
+    "url": "/Textutilis-/static/js/runtime~main.855b3516.js"
+  }
+]);
